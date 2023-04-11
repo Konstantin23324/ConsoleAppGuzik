ae99d75c3162ea3df677b28f891d2de80353bdf9
Константи Гузик <konstantinguzik7@gmail.com>
2023-04-11 19:11:26 +0300

    1 Commit

--- ConsoleAppGuzik/Program.cs
+++ ConsoleAppGuzik/Program.cs
@@ -10,6 +10,11 @@ namespace ConsoleAppGuzik
     {
         static void Main(string[] args)
         {
+            string s = Console.ReadLine();
+            double x = Convert.ToDouble(s);
+            double F = Math.Sin(x)+Math.Cos(x);
+            Console.WriteLine(x + " " + F);
+            Console.ReadLine();
         }
     }
 }
